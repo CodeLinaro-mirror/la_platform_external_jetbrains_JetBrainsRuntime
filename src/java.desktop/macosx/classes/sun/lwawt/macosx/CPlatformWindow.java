JetBrains/JetBrainsRuntime

JBR-2971 Log more information about window creation and property changes

src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformWindow.java
@@ -371,17 +371,32 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
 
                                 if (owner != null) {
                                     hasOwnerPtr = 0L != owner.executeGet(ownerPtr -> {
-                                        ref.set(nativeCreateNSWindow(viewPtr, ownerPtr, styleBits,
-                                                bounds.x, bounds.y,
-                                                bounds.width, bounds.height));
+                                        if (logger.isLoggable(PlatformLogger.Level.FINE)) {
+                                            logger.fine("nativeCreateNSWindow: owner=" + Long.toHexString(ownerPtr)
+                                                            + ", styleBits=" + Integer.toHexString(styleBits)
+                                                            + ", bounds=" + bounds);
+                                        }
+                                        long windowPtr = nativeCreateNSWindow(viewPtr, ownerPtr, styleBits,
+                                                bounds.x, bounds.y, bounds.width, bounds.height);
+                                        if (logger.isLoggable(PlatformLogger.Level.FINE)) {
+                                            logger.fine("window created: " + Long.toHexString(windowPtr));
+                                        }
+                                        ref.set(windowPtr);
                                         return 1;
                                     });
                                 }
 
                                 if (!hasOwnerPtr) {
-                                    ref.set(nativeCreateNSWindow(viewPtr, 0,
-                                            styleBits, bounds.x, bounds.y,
-                                            bounds.width, bounds.height));
+                                    if (logger.isLoggable(PlatformLogger.Level.FINE)) {
+                                        logger.fine("nativeCreateNSWindow: styleBits=" + Integer.toHexString(styleBits)
+                                                        + ", bounds=" + bounds);
+                                    }
+                                    long windowPtr = nativeCreateNSWindow(viewPtr, 0, styleBits,
+                                            bounds.x, bounds.y, bounds.width, bounds.height);
+                                    if (logger.isLoggable(PlatformLogger.Level.FINE)) {
+                                        logger.fine("window created: " + Long.toHexString(windowPtr));
+                                    }
+                                    ref.set(windowPtr);
                                 }
                             });
                             return ref.get();
@@ -581,7 +596,13 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
 
     // this is the counter-point to -[CWindow _nativeSetStyleBit:]
     private void setStyleBits(final int mask, final int value) {
-        execute(ptr -> nativeSetNSWindowStyleBits(ptr, mask, value));
+        execute(ptr -> {
+            if (logger.isLoggable(PlatformLogger.Level.FINE)) {
+                logger.fine("nativeSetNSWindowStyleBits: window=" + Long.toHexString(ptr)
+                        + ", mask=" + Integer.toHexString(mask) + ", value=" + Integer.toHexString(value));
+            }
+            nativeSetNSWindowStyleBits(ptr, mask, value);
+        });
     }
 
     private native void _toggleFullScreenMode(final long model);
@@ -730,8 +751,10 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
 
                     boolean isKeyWindow = CWrapper.NSWindow.isKeyWindow(ptr);
                     if (!isKeyWindow) {
+                        logger.fine("setVisible: makeKeyAndOrderFront");
                         CWrapper.NSWindow.makeKeyAndOrderFront(ptr);
                     } else {
+                        logger.fine("setVisible: orderFront");
                         CWrapper.NSWindow.orderFront(ptr);
                     }
 
@@ -1067,7 +1090,12 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
             execute(ptr -> nativeSynthesizeMouseEnteredExitedEvents(ptr, CocoaConstants.NSMouseExited));
         }
 
-        execute(ptr -> nativeSetEnabled(ptr, !blocked));
+        execute(ptr -> {
+            if (logger.isLoggable(PlatformLogger.Level.FINE)) {
+                logger.fine("nativeSetEnabled: window=" + Long.toHexString(ptr) + ", enabled=" + !blocked);
+            }
+            nativeSetEnabled(ptr, !blocked);
+        });
         checkBlockingAndOrder();
     }
 
@@ -1266,6 +1294,9 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
         pWindow.orderAboveSiblings();
 
         pWindow.execute(ptr -> {
+            if (logger.isLoggable(PlatformLogger.Level.FINE)) {
+                logger.fine("Focus blocker " + Long.toHexString(ptr));
+            }
             CWrapper.NSWindow.orderFrontRegardless(ptr);
             CWrapper.NSWindow.makeKeyAndOrderFront(ptr);
             CWrapper.NSWindow.makeMainWindow(ptr);

CodeLinaro-mirror/la_platform_external_jetbrains_JetBrainsRuntime

(cherry picked from commit 9d86b4d2358d7fe55d1706e2a6b22795f94658fa)

src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformWindow.java
@@ -366,17 +366,32 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
 
                                 if (owner != null) {
                                     hasOwnerPtr = 0L != owner.executeGet(ownerPtr -> {
-                                        ref.set(nativeCreateNSWindow(viewPtr, ownerPtr, styleBits,
-                                                bounds.x, bounds.y,
-                                                bounds.width, bounds.height));
+                                        if (logger.isLoggable(PlatformLogger.Level.FINE)) {
+                                            logger.fine("nativeCreateNSWindow: owner=" + Long.toHexString(ownerPtr)
+                                                            + ", styleBits=" + Integer.toHexString(styleBits)
+                                                            + ", bounds=" + bounds);
+                                        }
+                                        long windowPtr = nativeCreateNSWindow(viewPtr, ownerPtr, styleBits,
+                                                bounds.x, bounds.y, bounds.width, bounds.height);
+                                        if (logger.isLoggable(PlatformLogger.Level.FINE)) {
+                                            logger.fine("window created: " + Long.toHexString(windowPtr));
+                                        }
+                                        ref.set(windowPtr);
                                         return 1;
                                     });
                                 }
 
                                 if (!hasOwnerPtr) {
-                                    ref.set(nativeCreateNSWindow(viewPtr, 0,
-                                            styleBits, bounds.x, bounds.y,
-                                            bounds.width, bounds.height));
+                                    if (logger.isLoggable(PlatformLogger.Level.FINE)) {
+                                        logger.fine("nativeCreateNSWindow: styleBits=" + Integer.toHexString(styleBits)
+                                                        + ", bounds=" + bounds);
+                                    }
+                                    long windowPtr = nativeCreateNSWindow(viewPtr, 0, styleBits,
+                                            bounds.x, bounds.y, bounds.width, bounds.height);
+                                    if (logger.isLoggable(PlatformLogger.Level.FINE)) {
+                                        logger.fine("window created: " + Long.toHexString(windowPtr));
+                                    }
+                                    ref.set(windowPtr);
                                 }
                             });
                             return ref.get();
@@ -560,7 +575,13 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
 
     // this is the counter-point to -[CWindow _nativeSetStyleBit:]
     private void setStyleBits(final int mask, final int value) {
-        execute(ptr -> nativeSetNSWindowStyleBits(ptr, mask, value));
+        execute(ptr -> {
+            if (logger.isLoggable(PlatformLogger.Level.FINE)) {
+                logger.fine("nativeSetNSWindowStyleBits: window=" + Long.toHexString(ptr)
+                        + ", mask=" + Integer.toHexString(mask) + ", value=" + Integer.toHexString(value));
+            }
+            nativeSetNSWindowStyleBits(ptr, mask, value);
+        });
     }
 
     private native void _toggleFullScreenMode(final long model);
@@ -712,6 +733,7 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
 
                     boolean isKeyWindow = CWrapper.NSWindow.isKeyWindow(ptr);
                     if (!isKeyWindow) {
+                        logger.fine("setVisible: makeKeyWindow");
                         CWrapper.NSWindow.makeKeyWindow(ptr);
                     }
 
@@ -1048,7 +1070,12 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
             execute(ptr -> nativeSynthesizeMouseEnteredExitedEvents(ptr, CocoaConstants.NSMouseExited));
         }
 
-        execute(ptr -> nativeSetEnabled(ptr, !blocked));
+        execute(ptr -> {
+            if (logger.isLoggable(PlatformLogger.Level.FINE)) {
+                logger.fine("nativeSetEnabled: window=" + Long.toHexString(ptr) + ", enabled=" + !blocked);
+            }
+            nativeSetEnabled(ptr, !blocked);
+        });
     }
 
     public final void invalidateShadow() {
@@ -1234,6 +1261,9 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
         pWindow.orderAboveSiblings();
 
         pWindow.execute(ptr -> {
+            if (logger.isLoggable(PlatformLogger.Level.FINE)) {
+                logger.fine("Focus blocker " + Long.toHexString(ptr));
+            }
             CWrapper.NSWindow.orderFrontRegardless(ptr);
             CWrapper.NSWindow.makeKeyAndOrderFront(ptr);
             CWrapper.NSWindow.makeMainWindow(ptr);